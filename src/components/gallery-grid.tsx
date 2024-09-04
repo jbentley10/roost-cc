d0d40e2a9d25e07b6aa512104379bc1f4d0f91fc
Steam Deck User <deck@steamdeck.lan>
2024-09-04 16:42:39 -0700

    Allow the user to toggle and open images

--- src/components/gallery-grid.tsx
+++ src/components/gallery-grid.tsx
@@ -7,6 +7,9 @@ import {
   SelectValue,
 } from "./ui/select";
 import Image from "next/image";
+import { Dialog, DialogContent } from "./ui/dialog";
+import { Button } from "./ui/button";
+import { ChevronLeft, ChevronRight, X } from "lucide-react";
 
 type ViewType = "lgbtq" | "staff";
 type ImageType = {
@@ -27,6 +30,8 @@ function GalleryGrid(props: { images: ImageType[] }) {
   const { images } = props;
   const [selectedTag, setSelectedTag] = useState<string>("all");
   const [view, setView] = useState<ViewType>("lgbtq");
+  const [modalOpen, setModalOpen] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const handleViewChange = (value: ViewType) => {
     setView(value);
@@ -38,6 +43,27 @@ function GalleryGrid(props: { images: ImageType[] }) {
       : images.filter((image) =>
           image.contentfulMetadata.tags[0].name.includes(selectedTag)
         );
+
+  const openModal = (index: number) => {
+    setCurrentImageIndex(index);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const goToPreviousImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex > 0 ? prevIndex - 1 : filteredImages.length - 1
+    );
+  };
+
+  const goToNextImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex < filteredImages.length - 1 ? prevIndex + 1 : 0
+    );
+  };
 
   return (
     <>
@@ -68,20 +94,65 @@ function GalleryGrid(props: { images: ImageType[] }) {
         {/* Heading */}
         <h2 className={"text-center capitalize pb-8"}>{selectedTag}</h2>
 
-        <div className={""}>
-          <div className={"w-full"}>
-            {filteredImages.map((image: ImageType, index: number) => (
+        <div
+          className={
+            "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+          }
+        >
+          {filteredImages.map((image: ImageType, index: number) => (
+            <div
+              key={index}
+              className='cursor-pointer'
+              onClick={() => openModal(index)}
+            >
               <Image
-                key={index}
                 alt={image.image.description}
                 src={image.image.url}
                 width={300}
                 height={150}
+                className='object-cover w-full h-full'
               />
-            ))}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
+
+      <Dialog open={modalOpen} onOpenChange={setModalOpen}>
+        <DialogContent className='max-w-3xl w-full h-[80vh] flex items-center justify-center'>
+          <Button
+            variant='ghost'
+            size='icon'
+            className='absolute right-4 top-4'
+            onClick={closeModal}
+          >
+            <X className='h-4 w-4' />
+          </Button>
+          <div className='relative w-full h-full'>
+            <Image
+              alt={filteredImages[currentImageIndex]?.image.description || ""}
+              src={filteredImages[currentImageIndex]?.image.url || ""}
+              layout='fill'
+              objectFit='contain'
+            />
+            <Button
+              variant='ghost'
+              size='icon'
+              className='absolute left-4 top-1/2 transform -translate-y-1/2'
+              onClick={goToPreviousImage}
+            >
+              <ChevronLeft className='h-6 w-6' />
+            </Button>
+            <Button
+              variant='ghost'
+              size='icon'
+              className='absolute right-4 top-1/2 transform -translate-y-1/2'
+              onClick={goToNextImage}
+            >
+              <ChevronRight className='h-6 w-6' />
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
     </>
   );
 }
