5b7ea71b5b3053bb7490dbe2176a2441a244210a
John Bentley <jwbentle@gmail.com>
2025-01-16 08:16:08 -0800

    Bump copyright year

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -66,7 +66,7 @@ export const Footer = () => {
           "font-hand text-xl bg-primary text-white text-center pt-12 pb-10"
         }
       >
-        Copyright 2024{" "}
+        &copy;{`${new Date().getFullYear()}`}
         <Link
           className={"font-bold font-hand"}
           href='https://palmspringswebdesign.net'
