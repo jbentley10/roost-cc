0b143e8c2585e38eeb7aa034a4e76f896525e4d7
John Bentley <jwbentle@gmail.com>
2025-02-10 09:44:54 -0800

    Fix renderDocument

--- src/lib/renderDocument.tsx
+++ src/lib/renderDocument.tsx
@@ -5,9 +5,40 @@ import { type ReactNode, useState } from "react"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { BLOCKS } from "@contentful/rich-text-types"
 import { Button } from "@/components/ui/button"
+import Image from "next/image"
 
 export const renderDocument = (document: any) => {
-  // ... existing renderDocument code ...
+  const options = {
+    renderNode: {
+      [BLOCKS.EMBEDDED_ASSET]: (node: any) => (
+        <Image
+          src={`https:${node.data?.target?.fields?.file?.url}`}
+          alt={node.data?.target?.fields?.title}
+          width={node.data?.target?.fields?.file?.details?.image?.width}
+          height={node.data?.target?.fields?.file?.details?.image?.height}
+        />
+      ),
+      [BLOCKS.HEADING_4]: (node: any, children: ReactNode) => (
+        <h4 className='pb-6'>{children}</h4>
+      ),
+      [BLOCKS.PARAGRAPH]: (node: any, children: ReactNode) => (
+        <>
+          <p>{children}</p>
+          <br />
+        </>
+      ),
+      [BLOCKS.UL_LIST]: (node: any, children: ReactNode) => (
+        <ul className='pl-8'>{children}</ul>
+      ),
+      [BLOCKS.LIST_ITEM]: (node: any, children: ReactNode) => (
+        <li>{children}</li>
+      ),
+    },
+    renderText: (text: string) =>
+      text.split("\n").flatMap((text, i) => [i > 0 && <br key={i} />, text]),
+  };
+
+  return documentToReactComponents(document, options);
 }
 
 interface RenderShorthandProps {
